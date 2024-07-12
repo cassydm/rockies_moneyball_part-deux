dce30d29e3c471fecd42b7a86dff209abd585957
cassydm <cassydmiller@yahoo.com>
2024-07-11 21:19:47 -0600

    day 1 commit

---
+++ Test_files/Links.py
@@ -0,0 +1,70 @@
+# %% [markdown]
+# # 2024 Data links
+
+# %%
+# 2024 Batting link
+Team_Batting_2024_url="https://www.baseball-reference.com/teams/COL/2024.shtml#team_batting"
+# Cassy working on visualizations and data modifications for 2024 and 2007
+
+# %%
+# 2024 Team Pitching link
+Team_Pitching_2024_url="https://www.baseball-reference.com/teams/COL/2024.shtml#team_pitching"
+# Liz working on defining get_table function for pitching and possibly visuals for 2024 and 2007
+
+# %%
+# 2024 Team Fielding link
+Team_Fielding_2024_url="https://www.baseball-reference.com/teams/COL/2024-fielding.shtml#standard_fielding"
+# Howard working on defining get_table function for fielding and possibly visuals for 2024 and 2007
+
+# %%
+# 2024 Team Payroll link
+Team_Payroll_2024_url="https://www.baseball-reference.com/teams/COL/colorado-rockies-salaries-and-contracts.shtml#payroll"
+
+# %%
+# 2024 Players Value Batting link
+Players_Value_2024_url="https://www.baseball-reference.com/teams/COL/2024-batting.shtml#players_value_batting"
+
+# %%
+# 2024 Cumulative Batting link
+Cumulative_Batting_2024_url="https://www.baseball-reference.com/teams/COL/2024-batting.shtml#players_cumulative_batting"
+
+# %%
+# 2024 Full Season Roster link
+Team_Roster_2024_url="https://www.baseball-reference.com/teams/COL/2024.shtml#appearances"
+# Sandokan working on defining get_table function for roster and possible visuals for 2024 and 2007
+# %%
+# 2024 Players Value Pitching link
+Players_Value_Pitching_2024_url="https://www.baseball-reference.com/teams/COL/2024.shtml#players_value_pitching"
+
+# %% [markdown]
+# # 2007 Data links
+
+# %%
+# 2007 Batting Link
+Team_Batting_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#team_batting"
+
+# %%
+# 2007 Team Pitching link
+Team_Pitching_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#team_pitching"
+
+# %%
+# 2007 Team Fielding link
+Team_Fielding_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#standard_fielding"
+
+# %%
+# 2007 Full Season Roster link
+Team_Roster_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#appearances"
+
+# %%
+# 2007 Coaching Staff link
+Coaching_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#coaches"
+
+# %%
+# 2007 Players Value Batting link
+Players_Value_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#players_value_batting"
+
+# %%
+# 2007 Players Value Pitching link
+Players_Value_Pitching_2007_url="https://www.baseball-reference.com/teams/COL/2007.shtml#players_value_pitching"
+
+
